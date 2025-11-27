1d9405ee02e85ce30299d6b116b2650cb0ceaa57
zurek4320 <julekziolkowski01@gmail.com>
2025-11-27 16:36:39 +0100

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Clean old builds
+rm -f kasm.o kc.o kernel myos.iso
+
+# Assemble kernel
+nasm -f elf32 kernel.asm -o kasm.o
+
+# Compile C kernel
+gcc -m32 -ffreestanding -c kernel.c -o kc.o
+
+# Link everything
+ld -m elf_i386 -T link.ld -o kernel kasm.o kc.o
+
+cp kernel iso/boot/kernel
+
+# Create ISO for GRUB
+grub-mkrescue -o myos.iso iso
+
+# Boot in QEMU
+qemu-system-i386 -cdrom myos.iso
