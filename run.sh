41e379eebf4ecd98d9d9523399c71a570200d0f4
zurek4320 <julekziolkowski01@gmail.com>
2025-11-28 18:41:42 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Clean old builds
-rm -f kasm.o kc.o kernel myos.iso
+rm -f kasm.o kc.o kernel ZurOS.iso
 
 # Assemble kernel
 nasm -f elf32 kernel.asm -o kasm.o
@@ -12,10 +12,23 @@ gcc -m32 -ffreestanding -c kernel.c -o kc.o
 # Link everything
 ld -m elf_i386 -T link.ld -o kernel kasm.o kc.o
 
+# Copy kernel into ISO structure
 cp kernel iso/boot/kernel
 
-# Create ISO for GRUB
+# Create ISO (for bootloader)
 grub-mkrescue -o ZurOS.iso iso
 
-# Boot in QEMU
-qemu-system-i386 -cdrom ZurOS.iso
+# Create persistent HDD if missing
+if [ ! -f hdd.img ]; then
+    echo "Creating 64MB FAT32 disk image (hdd.img)..."
+    qemu-img create -f raw hdd.img 64M
+    mkfs.fat -F 32 hdd.img
+fi
+
+# Run QEMU with:
+#  - CD-ROM for booting
+#  - HDD for FAT32 persistent storage
+qemu-system-i386 \
+    -cdrom ZurOS.iso \
+    -drive file=hdd.img,format=raw,index=0,media=disk \
+    -boot d
